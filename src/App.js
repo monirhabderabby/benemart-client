192db6a565baca9d3615a6e4357a6ab531b8ea56
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-27 08:34:13 +0600

    project and css framework setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar fluid={true} rounded={true}>
+                <Navbar.Brand href="https://flowbite.com/">
+                    <img
+                        src="https://flowbite.com/docs/images/logo.svg"
+                        className="mr-3 h-6 sm:h-9"
+                        alt="Flowbite Logo"
+                    />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                        Flowbite
+                    </span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <Dropdown
+                        arrowIcon={false}
+                        inline={true}
+                        label={
+                            <Avatar
+                                alt="User settings"
+                                img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                                rounded={true}
+                            />
+                        }
+                    >
+                        <Dropdown.Header>
+                            <span className="block text-sm">Bonnie Green</span>
+                            <span className="block truncate text-sm font-medium">
+                                name@flowbite.com
+                            </span>
+                        </Dropdown.Header>
+                        <Dropdown.Item>Dashboard</Dropdown.Item>
+                        <Dropdown.Item>Settings</Dropdown.Item>
+                        <Dropdown.Item>Earnings</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item>Sign out</Dropdown.Item>
+                    </Dropdown>
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link href="/navbars" active={true}>
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">About</Navbar.Link>
+                    <Navbar.Link href="/navbars">Services</Navbar.Link>
+                    <Navbar.Link href="/navbars">Pricing</Navbar.Link>
+                    <Navbar.Link href="/navbars">Contact</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
+    );
 }
 
 export default App;
