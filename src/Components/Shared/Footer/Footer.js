3c8a28d1b81f80b62cfd4fd3808e9cc769d09675
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-06-29 07:33:52 +0600

    footer structure ready 80%

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons"
 const Footer = () => {
     return (
         <footer>
@@ -20,30 +21,44 @@ const Footer = () => {
                         <div className='mb-5'>
                             <h4>Usefull Links</h4>
                             <ul className='text-gray-500'>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight}/> <a href="" className='hover:text-primary'>Home</a></li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight}/> <a href="" className='hover:text-primary'>About</a></li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight}/> <a href="" className='hover:text-primary'>Poducts</a></li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight}/> <a href="" className='hover:text-primary'>Terms of services</a></li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight}/> <a href="" className='hover:text-primary'>Privacy policy</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Home</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>About</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Poducts</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Terms of services</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Privacy policy</a></li>
                             </ul>
                         </div>
                         <div className='mb-5'>
                             <h4>Our Service</h4>
                             <ul className='text-gray-500'>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} /> <a href="" className='hover:text-primary'>Shoe</a></li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} /> <a href="" className='hover:text-primary'>Shirt</a></li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} /> <a href="" className='hover:text-primary'>Pant</a> </li>
-                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} /> <a href="" className='hover:text-primary'>Ganji</a> </li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Shoe</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Shirt</a></li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Pant</a> </li>
+                                <li className='pb-4'><FontAwesomeIcon icon={faChevronRight} className="text-primary"/> <a href="" className='hover:text-primary'>Ganji</a> </li>
                             </ul>
                         </div>
                         <div className='mb-5'>
                             <h4>Join Our Newsletter</h4>
                             <p>Join 1m+ Products and never miss out leatest Fashion tips and more</p>
-                            <form>
-                                <input type="text" name="" />
-                                <button>Subscribe</button>
+                            <form className='flex flex-row flex-wrap'>
+                                <input type="text" name="" className='text-gray-500 w-2/3 p-2 focus:border-primary' placeholder='email@example.com'/>
+                                <button className='p-2 w-1/3 bg-primary text-white hover:bg-yellow-600'>Subscribe</button>
                             </form>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className='w-full bg-gray-900 text-gray-500 px-10'>
+                <div className='max-w-7xl flex flex-col sm:flex-row py-4 mx-auto justify-between items-center'>
+                    <div className='text-center'>
+                        <div>
+                            Copyright <strong><span>Company</span></strong>. All Rights Reserved
+                        </div>
+                    </div>
+                    <div className='text-white text-center text-xl'>
+                        <a href="" className='w-10 h-10 rounded-full bg-primary hover:bg-yellow-600 mx-1 inline-block pt-1'><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a href="" className='w-10 h-10 rounded-full bg-primary hover:bg-yellow-600 mx-1 inline-block pt-1'><FontAwesomeIcon icon={faTwitter} /></a>
+                        <a href="" className='w-10 h-10 rounded-full bg-primary hover:bg-yellow-600 mx-1 inline-block pt-1'><FontAwesomeIcon icon={faLinkedin} /></a>
                     </div>
                 </div>
             </div>
